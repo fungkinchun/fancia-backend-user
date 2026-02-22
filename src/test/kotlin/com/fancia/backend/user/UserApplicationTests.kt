1fb55dba1134e9dc22e37ad424155fafd71ad90e
fungkinchun <fungkinchun@gmail.com>
2026-02-22 01:23:26 +0000

    generated by spring initializr

---
+++ src/test/kotlin/com/fancia/backend/user/UserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fancia.backend.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
